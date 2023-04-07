cb207bfffd70544406e151245cccbeb6817de335
RamonSilvab <ramonsilvab05@gmail.com>
2023-04-07 20:04:43 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const novaTarefa = $('#nova-tarefa').val();
+        const listaTarefa = $('<li></li>');
+        $(listaTarefa).text(novaTarefa);
+        $(listaTarefa).appendTo('ul');
+
+        $('#nova-tarefa').val('');
+    })
+
+    $('ul').on('click', 'li', function() {
+        $(this).addClass("tarefa-concluida");
+    })
+})
+
